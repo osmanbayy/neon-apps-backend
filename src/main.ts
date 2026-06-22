b106c8be7dfc64dfd8f2eeacd6f728554763b196
Osman Bay <bayosman342@gmail.com>
2026-06-22 13:41:27 +0300

    feat: migrate to Fastify adapter and enhance TypeScript strictness
    - Updated main.ts to use FastifyAdapter for improved performance.
    - Configured application to listen on specified host and port from environment variables.
    - Modified tsconfig.json to enable strict mode and enforce noImplicitAny for better type safety.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  const host = process.env.HOST ?? '0.0.0.0';
+  await app.listen(port, host);
 }
-bootstrap();
+void bootstrap();
